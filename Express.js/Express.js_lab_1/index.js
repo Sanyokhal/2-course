0ed61e28cc9265767479f3f470e3f807ea323a1a
Sanyokhal <halsasha724@gmail.com>
2024-03-05 08:36:51 +0200

    reworked

--- Express.js/Express.js_lab_1/index.js
+++ Express.js/Express.js_lab_1/index.js
@@ -2,91 +2,93 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const fs = require("fs")
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+let temp_database = [
+    {
+        surname: "Hal",
+        room_num: 102,
+        section: "Wed dev",
+        pc_data: {
+            cpu: "i7-7000k",
+            gpu: "GTX-1660 TUF OC",
+            ram: 16
+        }
+    },
+    {
+        surname: "Kevpanych",
+        room_num: 104,
+        section: "Data analytics",
+        pc_data: {
+            cpu: "i9-9900k",
+            gpu: "RTX-3060",
+            ram: 32
+        }
+    },
+    {
+        surname: "Viktor",
+        room_num: 110,
+        section: "WEB DEV",
+        pc_data: {
+            cpu: "i9-9400f",
+            gpu: "4060TI",
+            ram: 64
+        }
+    }
+]
+
 
 // GET REQUESTS
 app.get('/', (req, res) => { // виведе всі об'єкти
-    fs.readFile(__dirname + '/' + 'workers.json', 'utf-8', (err, data) => {
-        var workers = JSON.parse(data);
-        res.end(JSON.stringify(workers));
-    })
+    res.end(JSON.stringify(temp_database))
 })
 app.get('/:id', (req, res) => { // виведе об'єкт з специфічним id
-    fs.readFile(__dirname + '/' + 'workers.json', 'utf-8', (err, data) => {
-        var workers = JSON.parse(data);
-        var worker = workers[req.params.id - 1]
-        res.end(JSON.stringify(worker));
-    })
+    res.end(JSON.stringify(temp_database[req.params.id - 1]))
 })
 
 
 // POST requests
 app.post('/', (req, res) => { // створить об'єкт
-    fs.readFile(__dirname + "/workers.json", 'utf8', (err, data) => {
-        let workers = JSON.parse(data);
-        const newWorker = {
-            "surname": req.body.surname,
-            "room_num": req.body.room_num,
-            "section": req.body.section,
-            "pc_data": {
-                "cpu": req.body.cpu,
-                "gpu": req.body.gpu,
-                "ram": req.body.ram
-            }
-        };
-        console.log(newWorker);
-        workers.push(newWorker);
-        fs.writeFile(__dirname + '/workers.json', JSON.stringify(workers, null, 2), 'utf8', (err) => {
-            if (err) {
-                res.status(500).send('Виникла помилка при створенні запису')
-            }
-            res.status(201).send('Створено успішно')
-        })
-    });
+    const newWorker = {
+        "surname": req.body.surname,
+        "room_num": req.body.room_num,
+        "section": req.body.section,
+        "pc_data": {
+            "cpu": req.body.cpu,
+            "gpu": req.body.gpu,
+            "ram": req.body.ram
+        }
+    };
+    console.log(newWorker);
+    res.status(201).send('Створено успішно')
 })
 app.post('/:id', (req, res) => { // редагує об'єкт з таким id
-    fs.readFile(__dirname + "/workers.json", 'utf8', (err, data) => {
-        let workers = JSON.parse(data);
-        const editedWorker = {
-            "surname": req.body.surname,
-            "room_num": req.body.room_num,
-            "section": req.body.section,
-            "pc_data": {
-                "cpu": req.body.cpu,
-                "gpu": req.body.gpu,
-                "ram": req.body.ram
-            }
-        };
-        workers[req.params.id - 1] = editedWorker
-        fs.writeFile(__dirname + '/workers.json', JSON.stringify(workers, null, 2), 'utf8', (err) => {
-            if (err) {
-                res.status(500).send('Виникла помилка при редагуванні запису')
-            }
-            res.status(201).send('Відредаговано успішно')
-        })
-    });
+    const editedWorker = {
+        "surname": req.body.surname,
+        "room_num": req.body.room_num,
+        "section": req.body.section,
+        "pc_data": {
+            "cpu": req.body.cpu,
+            "gpu": req.body.gpu,
+            "ram": req.body.ram
+        }
+    };
+    temp_database[req.params.id - 1] = editedWorker
+    res.status(201).send('Відредаговано успішно')
 })
 //DELETE REQUESTS
-app.delete('/:id', (req, res) => { // видалить об'єкт
-    fs.readFile(__dirname + "/workers.json", 'utf8', (err, data) => {
-        let workers = JSON.parse(data)
-        let new_workers = []
-        for (let worker in workers) {
-            if (worker !== (req.params.id - 1)) {
-                new_workers.push(workers[worker])
-            }
-        }
-        fs.writeFile(__dirname + '/workers.json', JSON.stringify(workers, null, 2), 'utf8', (err) => {
-            if (err) {
-                res.status(500).send('Виникла помилка при створенні запису')
-            }
-            res.status(200).send('Видалено')
-        })
-    });
+app.delete('/:name', (req, res) => { // видалить об'єкт
+    let result = temp_database.filter((worker) => worker.surname !== req.params.name)
+    if (result.length === temp_database.length) {
+        res.status(200).send('Не було видалено ні одного записа')
+    } else {
+        let diff = temp_database.length - result.length
+        temp_database = result
+        console.log(temp_database)
+        res.status(200).send(`Видалено ${diff} записів`)
+    }
 })
 app.listen(port, () => {
     console.log(`Прослуховую порт : ${port}`)
